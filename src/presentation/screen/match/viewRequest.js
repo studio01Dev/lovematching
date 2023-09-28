3d15de625f1ad32c4c843a4b397e91903e43e451
현근 기 <xanadu444444@gmail.com>
2023-09-28 20:25:32 +0900

    0928

--- src/presentation/screen/match/viewRequest.js
+++ src/presentation/screen/match/viewRequest.js
@@ -2,9 +2,46 @@ import people from '../../asset/images/people.svg'
 import arrow from '../../asset/images/back.png'
 import { Link, useParams } from 'react-router-dom';
 import ListItem from '../../component/input/list-item';
+import { useEffect, useState } from 'react';
+import ReadUserUseCase from '../../../domain/use_cases/readUser_useCase';
+import AdminSuggestListUseCase from '../../../domain/use_cases/adminSuggestList_usecase';
 
 export default function ViewRequest({ suggestList }) {
     const { uid } = useParams();
+    const [user, setUser] = useState(Object);
+    const [adminSuggestList, setAdminSuggestList] = useState(Array);
+    useEffect ( ()=> {
+        async function fetchOneUser() {
+            try {
+                const readUserUseCase = new ReadUserUseCase();
+                var response =  await readUserUseCase.readUser(uid)
+                console.log(response)
+                if(response.success === true) {
+                    setUser(response.data)
+                } else {
+                    alert(response.message)
+                }
+            } catch(error) {
+                alert('새로고침하거나, 번호를 다시 입력해주세요.')
+            }
+        }
+        async function fetchAdminSuggestListLength() {
+            try {
+                const adminSuggestList = new AdminSuggestListUseCase();
+                var response =  await adminSuggestList.readAdminSuggestList(uid)
+                console.log(response)
+                if(response.success === true) {
+                    setAdminSuggestList(response.data)
+                } else {
+                    alert(response.message)
+                }
+            } catch(error) {
+                alert('새로고침하거나, 번호를 다시 입력해주세요.')
+            }
+        }
+        fetchOneUser();
+        fetchAdminSuggestListLength();
+    }, [])
     return (
         <div>
 
