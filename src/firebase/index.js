cb17835fa3bd5a8a1ec7360faf858b7b81c35daa
taykim01 <taykim01@gmail.com>
2024-05-16 12:53:30 +0900

    fix: add env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,9 @@
-/* eslint-disable */
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDJ-j92XchVm_eEI0bt_fSbvIHgNV8-S5k",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "love-matching-ecb3c.firebaseapp.com",
   projectId: "love-matching-ecb3c",
   storageBucket: "love-matching-ecb3c.appspot.com",
@@ -20,11 +12,7 @@ const firebaseConfig = {
   measurementId: "G-YXQTZ6EF4S"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 const storage = getStorage(app);
 
